f8310177711ce71301a16e1680ff4b592150bbc1
willkao <willkao21@gmail.com>
2021-10-10 18:59:55 -0400

    Added scroll button to second page

--- mergetrip/src/components/HomePage.js
+++ mergetrip/src/components/HomePage.js
@@ -3,6 +3,7 @@ import './HomePage.css';
 import { withRouter } from 'react-router-dom';
 import leftBoarding from './BoardingLeft.svg'
 import rightBoarding from './BoardingRight.svg'
+import scrollButton from './ScrollButton.svg'
 
 class HomePage extends Component {
   constructor() {
@@ -17,10 +18,14 @@ class HomePage extends Component {
   redirect() {
     this.props.history.push('/account');
   }
+  /*
+  pageScroll() {
+    window.scrollBy (500, 500);
+  }*/
 
   render() {
     return (
-      <div className="HomePage">
+      <><div className="HomePage" id="PageOne">
         <img src={leftBoarding} className="Homepage-left" alt="boarding-left" />
         <img src={rightBoarding} className="Homepage-right" alt="boarding-right" />
         <style>
@@ -33,12 +38,22 @@ class HomePage extends Component {
           MERGETRIP
           <span className="HomePage-subtitle">No More Stress</span>
         </header>
+        <a className="HomePage-Scroll-Button" href="#pageTwo"><span></span>Button</a>
+
+        
       </div>
+      <div className="HomePage" id="pageTwo">
+        Second page going here later
+        <header className="HomePage-subtitle">MergeTrip can seemlesly create 
+          travel itineraries just by linking to your email
+          <a id="secondPage">See if this works</a>
+        </header>
+      </div></>
     );
   }
 
 }
 
-
+//<button className="HomePage-Scroll-Button" href="#secondPage">button</button>
 
 export default withRouter(HomePage);
