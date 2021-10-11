6dd77d7f7d582739b608ef3bd6830cbd23503e5d
willkao <willkao21@gmail.com>
2021-10-11 15:16:55 -0400

    Disable horizontal scroll and add fixed header

--- mergetrip/src/components/HomePage.js
+++ mergetrip/src/components/HomePage.js
@@ -20,33 +20,61 @@ class HomePage extends Component {
     this.props.history.push('/account');
   }
 
+  scroll() {
+    window.scrollBy(0, 8000);
+  }
+
+  toTop() {
+    window.scrollBy(0, -100000);
+  }
+
 
   render() {
     return (
-      <><div className="HomePage" id="PageOne">
+      <><div className="HomePage" id="HomePage">
         <img src={leftBoarding} className="Homepage-left" alt="boarding-left" />
         <img src={rightBoarding} className="Homepage-right" alt="boarding-right" />
         <style>
           @import url("https://use.typekit.net/osw3soi.css");
         </style>
-        <button className="HomePage-button" onClick={this.onSignUp}>
-          Sign Up
+       
+        <button className="home-button" onClick={this.toTop}>
+          MergeTrip
         </button>
-        <header className="HomePage-header">
+        <button className="HomePage-button" onClick={this.onSignUp}>
+          Log In / Sign Up
+        </button>
+        
+        <h1 className="HomePage-header">
           MERGETRIP
           <span className="HomePage-subtitle">No More Stress</span>
+          <button className="HomePage-Scroll-Button" onClick={this.scroll}>
+            <img src={downArrow}></img>
+          </button>
+        </h1>
+        
+        {
+          //<a className="HomePage-Scroll-Button" href="#pageTwo"><span></span><img src={downArrow}></img></a>
+        }
+        
+
+        <header className="HomePage-subtitle" id="pageTwo">MergeTrip can seemlesly create 
+          travel itineraries just by linking to your email
+          <a id="secondPage">See if this works</a>
         </header>
-        <a className="HomePage-Scroll-Button" href="#pageTwo"><span></span><img src={downArrow}></img></a>
 
         
       </div>
+      {/*}
       <div className="HomePage" id="pageTwo">
         Second page going here later
         <header className="HomePage-subtitle">MergeTrip can seemlesly create 
           travel itineraries just by linking to your email
           <a id="secondPage">See if this works</a>
         </header>
-      </div></>
+      </div>
+    */}
+      </>
     );
   }
 
