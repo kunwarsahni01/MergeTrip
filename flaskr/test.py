5d1a7f9feeb1bf5005d27371dbc1a9c53ba9c372
bansal51 <bansal51@purdue.edu>
2021-11-14 17:56:16 -0500

    filtering out actual dates from parsed data

--- flaskr/test.py
+++ flaskr/test.py
@@ -14,10 +14,10 @@ from pathlib import Path
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
-path = "C:\\Users\\yashy\\Documents\\MergeTrip\\flaskr\\delta.txt"
+path = "C:\\Users\\yashy\\Documents\\MergeTrip\\flaskr\\marriott.txt"
 text = Path(path).read_text()
-text = text.replace('\n', '')
-
+text = text.replace("\n","")
+text = text.replace("\t","")
 print(text)
           
 def add_regex_flag(vocab, pattern_str):
@@ -82,32 +82,6 @@ def time_regex(text):
     else:
         return ("none found")
 
-def date_regex_no_char(text):
-    pattern = r'(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d'
-    check = re.search(pattern, text)
-    date = re.findall(pattern, text)
-
-    for i in date:
-        print(i)
-
-    if check:
-        return ("found a match")
-    else:
-        return ("none found")
-
-def date_regex_char(text):
-    pattern = r'(January|February|March|April|May|June?|July|August|September|October|November|December)\s(\d\d?).+?(\d\d\d\d)'
-    check = re.search(pattern, text)
-    date = re.findall(pattern, text)
-
-    for i in date:
-        print(i)
-
-    if check:
-        return ("found a match")
-    else:
-        return ("none found")
-
 # using spacy to extract and classify data from text
 nlp = en_core_web_sm.load()
 doc = nlp(text)
@@ -115,29 +89,39 @@ pprint([(X.text, X.label_) for X in doc.ents])
 
 matches = []
 for i in doc.ents:
-  if i.label_ is 'DATE' and i.text not in matches:
+  if i.label_ == 'DATE' and i.text not in matches:
     matches.append(i.text)
     print(i.text)
 
+# filters for date parsing, works in format Month(3 char) day or DOTW, Month Day
+regex_date_filter_1 = "[A-Z][a-z]+, [A-Z][a-z]+ [1-9]|[12][0-9]|3[01]"
+regex_date_filter_2 = "[A-Z][a-z][a-z] [1-9]|[12][0-9]|3[01]"
+
+dates = []
+for match in matches:
+    if re.match(regex_date_filter_1, match) != None or re.match(regex_date_filter_2, match) != None:
+        dates.append(match)
+
+print(dates)
 
 # for the token pattern 1st, 22nd, 15th etc
-IS_REGEX_MATCH = add_regex_flag(nlp.vocab, '\d{1,2}(?:[stndrh]){2}?')
+#IS_REGEX_MATCH = add_regex_flag(nlp.vocab, '\d{1,2}(?:[stndrh]){2}?')
 
 # MM/DD/YYYY and YYYY/MM/DD
-pattern_1 = [{'IS_DIGIT': True}, {'ORTH': '/'}, {'IS_DIGIT': True}, {'ORTH': '/'}, {'IS_DIGIT': True}]
+#pattern_1 = [{'IS_DIGIT': True}, {'ORTH': '/'}, {'IS_DIGIT': True}, {'ORTH': '/'}, {'IS_DIGIT': True}]
 # MM-DD-YYYY and YYYY-MM-DD
-pattern_2 = [{'IS_DIGIT': True}, {'ORTH': '-'}, {'IS_DIGIT': True}, {'ORTH': '-'}, {'IS_DIGIT': True}]
+#pattern_2 = [{'IS_DIGIT': True}, {'ORTH': '-'}, {'IS_DIGIT': True}, {'ORTH': '-'}, {'IS_DIGIT': True}]
 # dates of the form 10-Aug-2018
-pattern_3 = [{'IS_DIGIT': True}, {'ORTH': '-'}, {'IS_ALPHA': True}, {'ORTH': '-'}, {'IS_DIGIT': True}]
+#pattern_3 = [{'IS_DIGIT': True}, {'ORTH': '-'}, {'IS_ALPHA': True}, {'ORTH': '-'}, {'IS_DIGIT': True}]
 # dates of the form Aug-10-2018
-pattern_4 = [{'IS_ALPHA': True}, {'ORTH': '-'}, {'IS_DIGIT': True}, {'ORTH': '-'}, {'IS_DIGIT': True}]
+#pattern_4 = [{'IS_ALPHA': True}, {'ORTH': '-'}, {'IS_DIGIT': True}, {'ORTH': '-'}, {'IS_DIGIT': True}]
 # dates of the form 10th August, 2018
 #pattern_5 = [{IS_REGEX_MATCH: True}, {'IS_ALPHA': True}, {'ORTH': ',', 'OP': '?'}, {'IS_DIGIT': True}]
 # dates of the form August 10th, 2018
 #pattern_6 = [{'IS_ALPHA': True}, {IS_REGEX_MATCH: True}, {'ORTH': ',', 'OP': '?'}, {'IS_DIGIT': True}]
-matcher = Matcher(nlp.vocab)
-patterns = [pattern_1, pattern_2, pattern_3, pattern_4]
-matcher.add("Date patterns", patterns)
+#matcher = Matcher(nlp.vocab)
+#patterns = [pattern_1, pattern_2, pattern_3, pattern_4]
+#matcher.add("Date patterns", patterns)
 
 #doc = nlp('Today is 06/11/2018 yesterday was 10-Jun-2018 and tomorrow is 06-12-2018 and I will go home on 7-Jul-2018 but clearly not on 39/02/2011 and some dates are of the form 12th February,2017')
 #matches = matcher(doc)
