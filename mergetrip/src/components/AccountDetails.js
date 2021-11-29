8898c927010624d6379ef5299000f4341d3e0c0f
gharrals <60074320+gharrals@users.noreply.github.com>
2021-11-29 00:30:55 -0500

    Updated new UI with Account Gmail Connection/

--- mergetrip/src/components/AccountDetails.js
+++ mergetrip/src/components/AccountDetails.js
@@ -1,30 +1,59 @@
-import React, { Component, useContext } from 'react';
+import React, { Component } from "react";
 import './AccountDetails.css';
 import { withRouter } from 'react-router-dom';
-import { getAuth, updateEmail, deleteUser, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, GoogleAuthProvider, signInWithPopup, OAuthProvider, updateProfile } from 'firebase/auth';
-import withAuthHOC from './withAuthHOC';
-// import leftBoarding from './BoardingLeft.svg'
-// import rightBoarding from './BoardingRight.svg'
-import { update } from '@firebase/database';
-import { AuthContext } from '../firebase';
-// import { initializeApp } from '@firebase/app';
-// import firebaseConfig from '../index.js'
+import { getAuth, updateEmail, deleteUser, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, GoogleAuthProvider, signInWithPopup, OAuthProvider, updateProfile } from "firebase/auth";
+import { getFirestore, setDoc, doc, addDoc, deleteDoc, collection } from "firebase/firestore";
+//import firebaseConfig from '../index.js'
 
-// const app = fireConfig.initializeApp(firebaseConfig);
+//const app = fireConfig.initializeApp(firebaseConfig);
 
 class AccountDetails extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
-      name: '',
-      email: '',
-      profileURL: '',
-      showUsernameText: false,
-      showEmailText: false,
-      newUsername: '',
-      newEmail: ''
+        name: "",
+        email: "",
+        profileURL: "",
+        showUsernameText:false,
+        showEmailText:false,
+        disconnected:false,
+        newUsername: "",
+        newEmail: ""
     };
 
+    //this.clickMenu = this.clickMenu.bind(this);
+    const auth = getAuth();
+    //console.log(auth.currentUser.displayName);
+    this.defaultName = "User";
+    this.defaultProfileURL = "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y&d=mp";
+
+    this.state.name = auth.currentUser.displayName;
+    if (this.state.name == null) {
+        this.state.name = this.defaultName;
+    }
+
+    this.state.profileURL = auth.currentUser.photoURL
+    if (this.state.profileURL == null) {
+        this.state.profileURL = this.defaultProfileURL;
+    }
+
+    this.state.email = auth.currentUser.email;
+    if (this.state.profileURL == null) {
+        this.state.profileURL = "No Email Found";
+    }
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+          this.loggedIn = true;
+        //   alert("logged!");
+        } else {
+          // No user is signed in.
+          this.loggedIn = false;
+        //   alert("Change!");
+        }
+      });
+
+    
     this.onChangeUsername = this.onChangeUsername.bind(this);
     this.onChangeEmail = this.onChangeEmail.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
@@ -32,178 +61,251 @@ class AccountDetails extends Component {
     this.showEmail = this.showEmail.bind(this);
     this.deleteAcount = this.deleteAcount.bind(this);
     this.deleteAccountCheck = this.deleteAccountCheck.bind(this);
+    this.disconnectAccount = this.disconnectAccount.bind(this);
+    this.connectAccount = this.connectAccount.bind(this);
   }
 
-  componentDidMount () {
-    console.log('componentDidMount: ', this.props);
+  
 
-    const auth = this.props.authState.user.auth;
-
-    const defaultName = 'User';
-    const defaultProfileURL = 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y&d=mp';
+  showEmail() {
+    this.setState({
+        showUsernameText: false
+    })
 
     this.setState({
-      name: auth.currentUser.displayName ? auth.currentUser.displayName : defaultName,
-      profileURL: auth.currentUser.photoURL ? auth.currentUser.photoURL : defaultProfileURL,
-      email: auth.currentUser.email
-    });
+        showEmailText: true
+    })
   }
 
-  showEmail () {
+  showUsername() {
     this.setState({
-      showUsernameText: false
-    });
+        showUsernameText: true
+    })
 
     this.setState({
-      showEmailText: true
-    });
+        showEmailText: false
+    })
   }
 
-  showUsername () {
-    this.setState({
-      showUsernameText: true
-    });
-
-    this.setState({
-      showEmailText: false
-    });
-  }
-
-  onChangeUsername () {
-    if (!document.getElementById('usernameTextbox.id')) {
-      console.log('NULL!');
-      return;
+  onChangeUsername() {
+    if (!document.getElementById("usernameTextbox.id")) {
+        console.log("NULL!");
+        return;
     }
 
-    console.log(document.getElementById('usernameTextbox.id').value);
-    const newUsername = document.getElementById('usernameTextbox.id').value;
-    // alert("Done!");
 
-    const auth = this.props.authState.user.auth;
+    const auth = getAuth();
+    if (!auth) {
+        alert("Auth NULL!!");
+        return;
+    }
 
-    updateProfile(auth.currentUser, { displayName: newUsername })
-      .then(() => {
-        alert('Username Changed Successfully');
-        this.setState({ newUsername: newUsername });
+    // if (!this.loggedIn) {
+    //     alert("logged??");
+    // }
+
+
+    if (auth.currentUser == null) {
+        alert("USER NULL!!");
+    }
+    console.log(document.getElementById("usernameTextbox.id").value);
+    this.state.newUsername = document.getElementById("usernameTextbox.id").value;
+    //alert("Done!");
+    
+    updateProfile(auth.currentUser, {displayName: this.state.newUsername})
+    .then(() => {
+        alert("Username Changed Successfully");
+        this.state.name = this.state.newUsername;
         this.showUsername();
         console.log(auth.currentUser.displayName);
-      }).catch((error) => {
+    }).catch((error) => {
         alert("Error: Couldn't change Username");
         console.log(error);
-      });
+    });
   }
 
-  onChangeEmail () {
-    const auth = this.props.authState.user.auth;
+  onChangeEmail() {
+    const auth = getAuth();
 
-    const newEmail = document.getElementById('emailTextbox.id').value;
+    this.state.newEmail = document.getElementById("emailTextbox.id").value;
 
-    updateEmail(auth.currentUser, newEmail)
+    updateEmail(auth.currentUser, this.state.newEmail)
       .then(() => {
-        alert('Email Changed');
+        alert("Email Changed");
         this.showEmail();
-        this.setState({ newEmail: newEmail });
+        this.state.email = this.state.newEmail;
         console.log(auth.currentUser.email);
       }).catch(function (error) {
         console.log(error);
       });
   }
 
-  onChangePassword () {
-    const auth = this.props.authState.user.auth;
+  onChangePassword() {
+    const auth = getAuth();
     sendPasswordResetEmail(auth, this.state.email)
       .then((userCredential) => {
-        alert('Password Reset Email Sent');
+        alert("Password Reset Email Sent");
       }).catch(function (error) {
         alert(error);
       });
   }
 
-  deleteAccountCheck () {
-    if (window.confirm('Are you sure you want to delete your account?')) {
-      this.deleteAcount();
+  deleteAccountCheck() {
+      if (window.confirm("Are you sure you want to delete your account?")) {
+          this.deleteAcount();
+      }
+  }
+
+  deleteAcount() {
+      const auth = getAuth();
+      deleteUser(auth.currentUser).then(() => {
+            alert("Account Deleted")
+            this.props.history.push('/');
+      }).catch((error) => {
+          alert("Delete Account Error")
+          console.log(error)
+      })
+  }
+
+  disconnectAccount() {
+    const auth = getAuth();
+    this.setState({disconnected : false});
+    const db = getFirestore();
+    const citiesRef = collection(db, "users");
+
+    try {
+      setDoc(doc(db, "users", auth.currentUser.uid), { 
+        googleToken: null,
+        test: 4321
+      });
+      console.log("Deletion of Google Token Successful")
+      alert("Disconnected Email Account")
+    } catch (e) {
+      console.error("Error adding document: ", e);
     }
   }
 
-  deleteAcount () {
-    const auth = this.props.authState.user.auth;
-    deleteUser(auth.currentUser).then(() => {
-      alert('Account Deleted');
-      this.props.history.push('/');
-    }).catch((error) => {
-      alert('Delete Account Error');
-      console.log(error);
-    });
+  connectAccount() {
+    this.setState({disconnected : true});
+
+    const provider = new GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/gmail.readonly');
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        const db = getFirestore();
+        try {
+          setDoc(doc(db, "users", auth.currentUser.uid), { 
+            googleToken: token,
+            test: 1234
+          });
+          console.log("Document written with ID: ", auth.currentUser.uid);
+        } catch (e) {
+          console.error("Error adding document: ", e);
+        }
+
+      }).catch((error) => {
+        // Handle Errors here.
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        // The email of the user's account used.
+        // const email = error.email;
+        // The AuthCredential type that was used.
+        // const credential = GoogleAuthProvider.credentialFromError(error);
+        alert("Incorrect Email or Password");
+
+        // ...
+      });
   }
 
-  render () {
-    console.log('render props: ', this.props);
+  render() {
     return (
-      <div className='AccountDetails'>
+      <div className="AccountDetails">
         <style>
-          @import url("https://use.typekit.net/osw3soi.css");
+            @import url("https://use.typekit.net/osw3soi.css");
         </style>
-        <header className='AccountDetails-header'>
-          <img src={this.state.profileURL} alt='profileImg' />
-          <div class='text'> Profile Information</div>
-          <div className='AccountDetails-subtitle'>
-            Username: {this.state.name}
+        <header className="AccountDetails-header">
+            <img src={this.state.profileURL} alt="profileImg" />
+            <div class="text"> Profile Information</div>
+            <div className="AccountDetails-subtitle">
+                Username: {this.state.name}
+                <br />
+                <br />
+                Email: {this.state.email}
             <br />
-            <br />
-            Email: {this.state.email}
-            <br />
-          </div>
-          <div>
+            </div>
+            <div>
 
-            <button className='AccountDetails-button' onClick={this.showUsername}>
-              Change Username
-            </button>
+                <button className="AccountDetails-button" onClick={this.showUsername}>
+                    Change Username
+                </button>
 
-            <button className='AccountDetails-button' onClick={this.showEmail}>
-              Change Email
-            </button>
+                <button className="AccountDetails-button" onClick={this.showEmail}>
+                    Change Email
+                </button>
 
-            <button className='AccountDetails-button' onClick={this.onChangePassword}>
-              Change Password
-            </button>
+                <button className="AccountDetails-button" onClick={this.onChangePassword}>
+                    Change Password
+                </button>
 
-            <br />
+                <br />
 
-            {this.state.showUsernameText
-              ? <button className='Reset-button' onClick={this.onChangeUsername}>
-                New Username
-                </button> : null}
+                {this.state.showUsernameText ? 
+                    <button className="Reset-button" onClick={this.onChangeUsername}>
+                        New Username
+                    </button>: null}
 
-            {this.state.showEmailText
-              ? <button className='Reset-button' onClick={this.onChangeEmail}>
-                New Email
-                </button> : null}
+                {this.state.showEmailText ? 
+                    <button className="Reset-button" onClick={this.onChangeEmail}>
+                        New Email
+                    </button>: null}
 
-            {this.state.showUsernameText
-              ? <input
-                // name="username"
-                  type='text'
-                  id='usernameTextbox.id'
-                  placeholder='Username'
-                /> : null}
+                {this.state.showUsernameText ? 
+                    <input 
+                        //name="username"
+                        type="text"
+                        id="usernameTextbox.id"
+                        placeholder="Username"
+                    /> : null}
 
-            {this.state.showEmailText
-              ? <input
-                  type='text'
-                  id='emailTextbox.id'
-                  placeholder='Email'
-                /> : null}
-          </div>
-          <div>
-            <button className='AccountDetails-button' onClick={this.deleteAccountCheck}>
-              Delete Account
-            </button>
-          </div>
+                {this.state.showEmailText ? 
+                    <input
+                        type="text" 
+                        id="emailTextbox.id"
+                        placeholder="Email"
+                    /> : null}
+            </div>
+            <div>
+            <button className="AccountDetails-button" onClick={this.deleteAccountCheck}>
+                    Delete Account
+                </button>
+            
+            {this.state.disconnected ?
+            <button className="AccountDetails-button" onClick ={this.disconnectAccount}>
+                    Disconnect Email
+               </button>
+             : null} 
+
+            {!this.state.disconnected ?
+            <button className="AccountDetails-button" onClick ={this.connectAccount}>
+                    Connect Email
+               </button>
+             : null} 
+
+            </div>
 
         </header>
       </div>
     );
   }
+
 }
 
-export default withRouter(withAuthHOC(AccountDetails));
+
+
+export default withRouter(AccountDetails);
