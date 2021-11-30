74c147dae0de76d24c664100843431bef68d6ffc
IbrahimSaeedPurdue <54752803+IbrahimSaeedPurdue@users.noreply.github.com>
2021-11-30 14:50:45 -0500

    updated group/view members to use new  fetchTrips

--- mergetrip/src/components/Groups.js
+++ mergetrip/src/components/Groups.js
@@ -1,13 +1,13 @@
-import './Groups.css'
+import './Groups.css';
 import React, { Component, useState, useEffect } from 'react';
-//import { withRouter } from 'react-router';
-//import { getAuth } from "firebase/auth";
+// import { withRouter } from 'react-router';
+// import { getAuth } from "firebase/auth";
 import { doc, getDoc, getFirestore, deleteDoc, setDoc, getDocs, collection, updateDoc } from 'firebase/firestore';
-//import withAuthHOC from './withAuthHOC';
+// import withAuthHOC from './withAuthHOC';
 import CreateGroup from './CreateGroup';
 import { useAuthState } from '../firebase';
 import Reservation from '../pages/Reservation';
-import {getTrips} from '../api/flaskr_api';
+import { getTrips } from '../api/flaskr_api';
 import SwitchGroup from './SwitchGroup';
 import JoinGroup from './JoinGroup';
 import ViewMember from './ViewMember';
@@ -74,177 +74,179 @@ class Groups extends Component {
       this.props.setCurrentPage(<JoinGroup />);
   }
 */
-const Groups = ({setCurrentPage}) => {
-    const auth = useAuthState();
-    const [groupName, setGroupName] = useState(false);
-    const [memberUid, setMemUid] = useState('');
-    const [inviteUid, setInviteUid] = useState('');
-    const [trips, setTrips] = useState(false);
-    const [showTrips, setShowTrips] = useState(false);
+const Groups = ({ setCurrentPage }) => {
+  const auth = useAuthState();
+  const [groupName, setGroupName] = useState(false);
+  const [memberUid, setMemUid] = useState('');
+  const [inviteUid, setInviteUid] = useState('');
+  const [trips, setTrips] = useState(false);
+  const [showTrips, setShowTrips] = useState(false);
 
-    const onLeave = async () => {
-        //const auth = this.props.authState.user.auth;
-        const db = getFirestore();
-        const uIdDocSnap = await getDoc(doc(db, "users", auth.user.uid));
-        const groupName = uIdDocSnap.get("group");
-        if (groupName != null) {
-            const docRef = doc(db, `groups/${groupName}/members`, auth.user.uid);
-        await deleteDoc(docRef);
-        updateDoc(doc(db, "users", auth.user.uid), {
-            group: ""
-        });
-        var count = 0;
-        const querySnap = await getDocs(collection(db, `groups/${groupName}/members`));
-        querySnap.forEach((doc) => {
-            count++;
-        });
-        if (count == 0) {
-            await deleteDoc(doc(db, "groups", groupName));
-        }
-          alert("Successfully left group");
-        } else {
-          alert("Unable to leave group")
-        }
+  const onLeave = async () => {
+    // const auth = this.props.authState.user.auth;
+    const db = getFirestore();
+    const uIdDocSnap = await getDoc(doc(db, 'users', auth.user.uid));
+    const groupName = uIdDocSnap.get('group');
+    if (groupName != null) {
+      const docRef = doc(db, `groups/${groupName}/members`, auth.user.uid);
+      await deleteDoc(docRef);
+      updateDoc(doc(db, 'users', auth.user.uid), {
+        group: ''
+      });
+      let count = 0;
+      const querySnap = await getDocs(collection(db, `groups/${groupName}/members`));
+      querySnap.forEach((doc) => {
+        count++;
+      });
+      if (count == 0) {
+        await deleteDoc(doc(db, 'groups', groupName));
+      }
+      alert('Successfully left group');
+    } else {
+      alert('Unable to leave group');
     }
+  };
 
-    const onInvite = async () => {
-        if (inviteUid === "") {
-            alert("Please input the user Id that you would like to invite");
-        } else {
-            //Invite other users
-            const db = getFirestore();
-            const docref = doc(db, "users", `${inviteUid}`);
-            const docSnap = await getDoc(docref);
-            if (docSnap.exists()) {
-                //Add user to invited list
-                const uIdDocSnap = await getDoc(doc(db, "users", auth.user.uid));
-                const groupName = uIdDocSnap.get("group");
-                setDoc(doc(db, `groups/${groupName}/invited`, inviteUid), {
-                    uid: inviteUid
-                });
-                alert("Successfully invited user");
-            } else {
-                alert("No user with the userId: " + `${inviteUid}`);
-            }            
-        }
-    }
-
-    const onCreate = async () => {
-        //setCurrentPage(<CreateGroup />);
-    }
-
-    useEffect(() => {
-        const userId = auth.user.uid;
-        if (!groupName) getGroupName(userId);
-        if (!trips) fetchTrips(userId);
-    }, []);
-
-    const fetchTrips = async (userId) => {
-        console.log('Fetching trips:\n');
-
-        const res = await getTrips(userId);
-        setTrips(res.data.trips);
-    };    
-
-    const getGroupName = async (userId) => {
+  const onInvite = async () => {
+    if (inviteUid === '') {
+      alert('Please input the user Id that you would like to invite');
+    } else {
+      // Invite other users
       const db = getFirestore();
-      const docref = doc(db, 'users', userId);
-      const docSnap = await(getDoc(docref));
-      if (docSnap.exists) {
-        //get groupName field from docsnap
-        const group = docSnap.get("group");
-        if (group != null) {
-          setGroupName(group);
-        }
+      const docref = doc(db, 'users', `${inviteUid}`);
+      const docSnap = await getDoc(docref);
+      if (docSnap.exists()) {
+        // Add user to invited list
+        const uIdDocSnap = await getDoc(doc(db, 'users', auth.user.uid));
+        const groupName = uIdDocSnap.get('group');
+        setDoc(doc(db, `groups/${groupName}/invited`, inviteUid), {
+          uid: inviteUid
+        });
+        alert('Successfully invited user');
+      } else {
+        alert('No user with the userId: ' + `${inviteUid}`);
       }
     }
+  };
 
-  //render() {
-    return (
-      <>
-        <style>
-            @import url("https://use.typekit.net/osw3soi.css");
-        </style>
-        <br/>
-        <header class="text">
-          {
-            groupName
-              ? <p>Current Group: {groupName}</p>
-              : <p>Join a group first</p>
-          }    
-        </header>
+  const onCreate = async () => {
+    // setCurrentPage(<CreateGroup />);
+  };
 
-        <h2>
-            <button class="leave-group-button" onClick={onLeave}>
-                Leave Group
-            </button>
-            <button class="create-button" onClick={() => {setCurrentPage(<CreateGroup setCurrentPage={setCurrentPage}/>)}}>
-                Create Group
-            </button>
-            <button class="switch-button" onClick={() => {setCurrentPage(<SwitchGroup setCurrentPage={setCurrentPage}/>)}}>
-                Switch Groups
-            </button>
-            <button class="join-button" onClick={() => {setCurrentPage(<JoinGroup setCurrentPage={setCurrentPage}/>)}}>
-                Join Group
-            </button>
-            <br/>
-        </h2>
+  useEffect(() => {
+    const userId = auth.user.uid;
+    if (!groupName) getGroupName(userId);
+    if (!trips) fetchTrips(userId);
+  }, []);
+
+  const fetchTrips = async (userId) => {
+    console.log('Fetching trips:\n');
+
+    const res = await getTrips(userId);
+    setTrips(res);
+  };
+
+  const getGroupName = async (userId) => {
+    const db = getFirestore();
+    const docref = doc(db, 'users', userId);
+    const docSnap = await (getDoc(docref));
+    if (docSnap.exists) {
+      // get groupName field from docsnap
+      const group = docSnap.get('group');
+      if (group != null) {
+        setGroupName(group);
+      }
+    }
+  };
+
+  // render() {
+  return (
+    <>
+      <style>
+        @import url("https://use.typekit.net/osw3soi.css");
+      </style>
+      <br />
+      <header class='text'>
+        {
+          groupName
+            ? <p>Current Group: {groupName}</p>
+            : <p>Join a group first</p>
+        }
+      </header>
+
+      <h2>
+        <button class='leave-group-button' onClick={onLeave}>
+          Leave Group
+        </button>
+        <button class='create-button' onClick={() => { setCurrentPage(<CreateGroup setCurrentPage={setCurrentPage} />); }}>
+          Create Group
+        </button>
+        <button class='switch-button' onClick={() => { setCurrentPage(<SwitchGroup setCurrentPage={setCurrentPage} />); }}>
+          Switch Groups
+        </button>
+        <button class='join-button' onClick={() => { setCurrentPage(<JoinGroup setCurrentPage={setCurrentPage} />); }}>
+          Join Group
+        </button>
+        <br />
+      </h2>
+      <br />
+      <br />
+      <div>
+        <input
+          class='invite-input'
+          name='inviteUid'
+          type='text'
+          value={inviteUid}
+          placeholder='Enter the User Id to invite'
+          onChange={e => setInviteUid(e.target.value)}
+        />
+        <br />
+        <button class='invite-button' onClick={onInvite}>
+          Invite users
+        </button>
         <br />
         <br />
-        <div>
-            <input class="invite-input"
-              name="inviteUid"
-              type="text"
-              value={inviteUid}
-              placeholder="Enter the User Id to invite"
-              onChange={e => setInviteUid(e.target.value)}
-            />
-            <br/>
-            <button class="invite-button" onClick={onInvite}>
-              Invite users
-            </button>
-            <br/>
-            <br/>
-            <br/>
-            <p>Enter the User ID of the group member to view their itinerary</p>
-            <input class="group-input"
-                name="viewUid"
-                type="text"
-                value={memberUid}
-                onChange={e => setMemUid(e.target.value)}
-            />
-            <br/>
-            {/*Need to also pass groupName*/}
-            <button class="view-button" onClick={() => {setCurrentPage(<ViewMember viewId={memberUid} setCurrentPage={setCurrentPage}/>); }}>
-                View
-            </button>
+        <br />
+        <p>Enter the User ID of the group member to view their itinerary</p>
+        <input
+          class='group-input'
+          name='viewUid'
+          type='text'
+          value={memberUid}
+          onChange={e => setMemUid(e.target.value)}
+        />
+        <br />
+        {/* Need to also pass groupName */}
+        <button class='view-button' onClick={() => { setCurrentPage(<ViewMember viewId={memberUid} setCurrentPage={setCurrentPage} />); }}>
+          View
+        </button>
+        {
+          trips
+            ? trips.map((trip, index) => (
+              <div key={index} className='Trip-container'>
+                <div className='Trip-header'>
+                  <p>{trip.trip_name}</p>
+                </div>
+                <div className='Trip-body'>
+                  <p>Start: {trip.start_date}</p>
+                  <p>End: {trip.end_date}</p>
+                </div>
+                <p>Reservations:</p>
+                <button className='Trip-button' type='button' onClick={() => { setShowTrips(prevShow => !prevShow); }}>Toggle Reservations</button>
                 {
-                trips  
-                    ? trips.map((trip, index) => (
-                        <div key={index} className='Trip-container'>
-                            <div className='Trip-header'>
-                                <p>{trip.trip_name}</p>
-                            </div>
-                            <div className='Trip-body'>
-                                <p>Start: {trip.start_date}</p>
-                                <p>End: {trip.end_date}</p>
-                            </div>
-                            <p>Reservations:</p>
-                            <button className='Trip-button' type='button' onClick={() => { setShowTrips(prevShow => !prevShow); }}>Toggle Reservations</button>
-                            {
-                                showTrips && trip.reservations.length !== 0
-                                    ? trip.reservations.map((res, index) => <Reservation fetchTrips={fetchTrips} res={res} userId={trip.user_id} tripId={trip.trip_id} key={index} />)
-                                    : <p>Click the button to hide/show your reservations</p>
-                            }
-                        </div>
-                    ))
-                    : <p>No Trips planned</p>
+                  showTrips && trip.reservations.length !== 0
+                    ? trip.reservations.map((res, index) => <Reservation fetchTrips={fetchTrips} res={res} userId={trip.user_id} tripId={trip.trip_id} key={index} />)
+                    : <p>Click the button to hide/show your reservations</p>
                 }
+              </div>
+              ))
+            : <p>No Trips planned</p>
+        }
 
-        </div>
-      </>
-    );
- // }
-}
-//export default withRouter(withAuthHOC(Groups));
+      </div>
+    </>
+  );
+  // }
+};
+// export default withRouter(withAuthHOC(Groups));
 export default Groups;
