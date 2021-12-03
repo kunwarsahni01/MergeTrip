6c74ca73d40d0ccddefcf7f4b8e7a0f8de03c9c2
willkao <54589298+willkao@users.noreply.github.com>
2021-12-03 14:46:02 -0500

    uncomment

--- mergetrip/src/components/Login.js
+++ mergetrip/src/components/Login.js
@@ -75,7 +75,7 @@ export class Login extends Component {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
         // The signed-in user info.
-        //const user = result.user;
+        const user = result.user;
         const db = getFirestore();
         const userId = auth.currentUser.uid;
         updateDoc(doc(db, 'users', userId), {
